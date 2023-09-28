0ed98275c86927f58a510cf944ac27df4357ddc0
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-09-28 12:07:55 +0200

    Pequeñas anotaciones y/o comentarios

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     const calculateButton = document.getElementById("calculate");
     const resultContainer = document.getElementById("result");
 
-    calculateButton.addEventListener("click", function () {
+    calculateButton.addEventListener("click",  () => {
         const length = parseFloat(document.getElementById("length").value);
         const width = parseFloat(document.getElementById("width").value);
 
+        /**
+         * Si alguno de los dos valores introducidos por el usuario no es un número
+         */
         if (!isNaN(length) && !isNaN(width)) {
             const area = length * width;
             resultContainer.innerHTML = `El área del rectángulo es: ${area}`;
