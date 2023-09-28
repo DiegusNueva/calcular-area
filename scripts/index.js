97bf93c4bcabd4e9df212c166fc4d7176db3ebbd
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-09-28 12:18:00 +0200

    Añadimos más comentarios al ejercicio para hacerlo más inteligible.

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,22 @@
+// Primero, que se cargue todo el HTML y el CSS.
 document.addEventListener("DOMContentLoaded", () => {
     const calculateButton = document.getElementById("calculate");
     const resultContainer = document.getElementById("result");
 
     calculateButton.addEventListener("click",  () => {
+        // Largo
         const length = parseFloat(document.getElementById("length").value);
+        // Ancho
         const width = parseFloat(document.getElementById("width").value);
 
         /**
-         * Si alguno de los dos valores introducidos por el usuario no es un número
+         *  Si el valor del largo es un número válido Y el valor del ancho es un número válido.
          */
         if (!isNaN(length) && !isNaN(width)) {
+            // Multiplicamos largo por ancho y obtenemos el área.
             const area = length * width;
+            // Añadimos el resultado al div que tenemos en el template.
+            //Nos vamos acostumbrando a la plantilla literal o template literal.
             resultContainer.innerHTML = `El área del rectángulo es: ${area}`;
         } else {
             resultContainer.innerHTML = "Por favor, ingrese valores válidos.";
